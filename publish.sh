61eaf252dc057f91dfddbe38fc39aab78dcc7453
Canius Chu <canius.chu@outlook.com>
2023-03-29 16:44:54 +0800

    update scripts

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-declare DEST_DIR=$1
-declare PUB_DIR=".pub"
+DEST_DIR="$1"
+PUB_DIR=".pub"
 
 set -e
 
@@ -11,13 +11,13 @@ cd ../service
 swift build -c release
 
 cd ../
-rm -rf $PUB_DIR
-mkdir $PUB_DIR
+rm -rf "$PUB_DIR"
+mkdir "$PUB_DIR"
 
-mv service/Public $PUB_DIR/Public
-mv service/.build/release/Run $PUB_DIR/Run
-mv service/.build/release/Run.dSYM $PUB_DIR/Run.dSYM
+mv service/Public "$PUB_DIR/Public"
+mv service/.build/release/Run "$PUB_DIR/Run"
+mv service/.build/release/Run.dSYM "$PUB_DIR/Run.dSYM"
 
-mkdir -p $DEST_DIR
-cp -rf $PUB_DIR/* $DEST_DIR
-rm -rf $PUB_DIR
+mkdir -p "$PUB_DIR"
+cp -rf "$PUB_DIR/*" "$DEST_DIR"
+rm -rf "$PUB_DIR"
